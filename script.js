d05e34d220f46c0761106317c6bdca540e117f11
Pedro Furtado Farias <159261586+pedrofurtadof@users.noreply.github.com>
2024-07-31 10:23:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let lamp=  document.getElementById("imagem")
+let legendas= document.getElementById("legenda")
+function imagem1(){
+
+    lamp.src = '1.png'
+    legendas.innerHTML= 'Final de tarde da cidade'
+    
+}
+function imagem2(){
+
+    lamp. src = '2.png'
+    legendas.innerHTML= 'Visão aérea da cidade'
+    
+}
+
+function imagem3(){
+
+    lamp.src = '3.png'
+    legendas.innerHTML= 'Começo da tarde da cidade'
+}
+function imagem4(){
+
+    lamp. src = '4.png'
+    legendas.innerHTML= 'Visão do mar para cidade'
+}
